8e4ceb0c55cb4a4a2657afebd7ff1e2df74db77a
Franz Scharnreitner <fscharnreitner@gmail.com>
2026-04-14 09:17:08 +0200

    updated scoring function

--- src/optim/oscillatory_optim.jl
+++ src/optim/oscillatory_optim.jl
@@ -20,7 +20,7 @@ function objective(x, p)
 
     sol = Dynamic.solve_model(model)
 
-    return - oscillation_score(sol.paths[:Y]) + positivity_penalty(sol.paths)
+    return - oscillation_score(sol.paths[:Y]) + positivity_penalty(sol.paths) + explosion_penalty(sol.paths)
 end
 
 
@@ -42,12 +42,12 @@ function eval_model(x, p)
     return sol
 end
 
+function explosion_penalty(y)
+    return sum(max(0, abs(xi) - 100)^2 for xi in reduce(vcat, values(y)))
+end
 
 function oscillation_score(y)
-
-
     # Penalty for negative values
-    penalty_explosion = sum(max(0, abs(xi) - 100)^2 for xi in y)
     x = y[50:end]
 
     μ = mean(x)
@@ -61,11 +61,20 @@ function oscillation_score(y)
 
     # Penalty for explosion (values far from zero)
 
-    return crossings + sign_changes - 100 * penalty_explosion
+    return crossings + sign_changes
 end
 
 
+p = Dict(
+    :x => [0.3, 10.5],
+    :y => [0.3, 0.5]
+)
+
+positivity_penalty(p)
+
+
 x0 = [0.008905350496328067, 0.037493422061853576, 0.48152300038241813, 0.24028386950622796, 0.11548465017702524, 0.17509632134056813, 0.35048918515442207, 0.9455059561651523, 0.6241246752385647, 0.044024262656295665]
+x1 = [0.8463213035143698, 0.2550484113257094, 0.052025500689039134, 0.3184621806282828, 0.06425994550113504, 0.5006502538028674, 0.9706631007613131, 0.0005959482135358993, 0.29553658119280646, 0.31639299459056147]
 
 function callback(state, loss)
     println("Iteration: $(state.iter), Loss: $loss, Params: $(state.u)")
