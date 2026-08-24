762dbccde4f6a162b1593b7c35cb1cfd06c21949
Franz Scharnreitner <fscharnreitner@gmail.com>
2026-08-24 13:00:11 +0200

    deleted stale files

--- src/dash.jl
+++
@@ -1,8 +0,0 @@
-using PKAssetPrices
-
-models = PKAssetPrices.dashboard_models()
-dynamic_models = PKAssetPrices.dashboard_dynamic_models()
-app = Dashboard.get_app(models, dynamic_models)
-Dashboard.register_callbacks!(app, models, dynamic_models)
-
-Dashboard.run(app)
